bdca09849ec2b90a5b6a3f32223eef407558c6ff
vinodh1988 <k.c.vinodh@hotmail.com>
2021-12-09 16:44:03 +0530

    route-gaurds

---
+++ src/app/Guards/log.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from '../services/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogGuard implements CanActivate {
+  
+  constructor(private ls:LoginService){
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(this.ls.getStatus())
+      return true;
+    return false;
+  }
+  
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogGuard } from './Guards/log.guard';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
    {path:"",component: HomeComponent},
-   {path:"about",component: AboutComponent},
+   {path:"about",component: AboutComponent,canActivate:[LogGuard]},
    {path:"contact",component: ContactComponent},
+  
 ];
 
 @NgModule({
